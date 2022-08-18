b705ac1b1f0e228ff9b3ce674bc2dd75348570ca
NanoBibiani <nbibiani70@gmail.com>
2022-08-18 19:27:07 -0300

    creacion de Footer

---
+++ sprint-1/src/componentes/Footer.js
@@ -0,0 +1,17 @@
+export default function Footer () {
+    return (
+        <div className = "Footer">
+          <a href="" class="fa fa-home" aria-hidden="true">home</a>
+            <div className = "socialMedia">
+              <h3>You can also follow us social media</h3>
+                <div className = "socialMedia1">
+                 <a href="">Facebook</a>
+                 <a href="">Instagram</a>
+                 <a href="">Whatsapp</a>
+                </div>
+            </div>
+          <a href="" class="fa fa-plane" aria-hidden="true">plane</a>
+        </div>
+    )
+}
+
