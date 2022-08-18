4346a096c3f68d31b6f0c7a7cad4f0aef66f8a84
NanoBibiani <nbibiani70@gmail.com>
2022-08-18 19:25:42 -0300

    modificion App.js

---
+++ sprint-1/src/pages/App.js
@@ -0,0 +1,15 @@
+import Footer from '../componentes/Footer';
+import Welcome from '../componentes/Welcome';
+import '../styles/footer.css'
+import '../styles/welcome.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Welcome/>
+     <Footer/>
+    </div>
+  );
+}
+
+export default App;
