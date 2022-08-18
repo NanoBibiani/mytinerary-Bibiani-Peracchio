1738231c9bd02aa582881b7ec44399afc7758594
NanoBibiani <nbibiani70@gmail.com>
2022-08-18 19:27:43 -0300

    creacion Welcome.js

---
+++ sprint-1/src/componentes/Welcome.js
@@ -0,0 +1,9 @@
+export default function Welcome(){
+    return (
+        <div className = "welcome">
+            <h1>App MyTernary</h1>
+            <p className = "slogan">The only rule of the trip is: don't come back as you left. come back different</p>
+            <button>Go to App</button>
+        </div>
+    )
+}
