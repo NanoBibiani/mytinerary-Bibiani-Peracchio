12e1e637f5df789c3761d037916320296ec07fa4
NanoBibiani <nbibiani70@gmail.com>
2022-08-19 19:49:56 -0300

    creacion de header

---
+++ sprint-1/src/componentes/Header.js
@@ -0,0 +1,8 @@
+export default function Header () {
+    return (
+        <div className = "Header">
+            <img className = "logoViaje" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5z8e5bsNhF0CSQhzHBTC_DBPmcJAe9zMwIw&usqp=CAU" alt="Logo"></img>
+            <h2>Home Page</h2>
+        </div>
+    )
+}
