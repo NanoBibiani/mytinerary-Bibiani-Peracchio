e62beb85edea2817d0a9286eebceec38b9158ebe
jojope38 <peracchiojoaquin16@gmail.com>
2022-08-21 19:00:58 -0300

    underConstruction

---
+++ sprint-1/src/pages/Under.js
@@ -0,0 +1,11 @@
+import SitioWebLayout from '../layouts/SitioWebLayout';
+
+export default function UnderConstruction () {
+    return (
+        <div>
+            <SitioWebLayout>
+                <h2>Under Construction</h2>
+            </SitioWebLayout>
+        </div>
+    )
+}
