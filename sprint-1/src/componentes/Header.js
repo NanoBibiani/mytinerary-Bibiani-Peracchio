635d7c3252da7971b5b0f5ab54c0f4241f989ca0
NanoBibiani <nbibiani70@gmail.com>
2022-08-23 00:07:32 -0300

    modificacion de logo

--- sprint-1/src/componentes/Header.js
+++ sprint-1/src/componentes/Header.js
@@ -1,8 +1,7 @@
 export default function Header () {
     return (
         <div className = "Header">
-            <img className = "logoViaje" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5z8e5bsNhF0CSQhzHBTC_DBPmcJAe9zMwIw&usqp=CAU" alt="Logo"></img>
-            <h2>Home Page</h2>
+            <img className = "logoViaje" src="http://localhost:3000/logosinfondo.png" alt="Logo"></img>
         </div>
     )
 }
