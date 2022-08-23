defcae982c16706486921691f991f416c45dfda1
NanoBibiani <nbibiani70@gmail.com>
2022-08-23 00:09:02 -0300

    prueba Cities

--- sprint-1/src/pages/App.js
+++ sprint-1/src/pages/App.js
@@ -3,23 +3,27 @@ import Welcome from '../componentes/Welcome';
 import Header from '../componentes/Header';
 import SitioWebLayout from '../layouts/SitioWebLayout';
 import HomePage from './HomePage';
+import EventCarousel from '../componentes/EventCarousel'
+import Cities from './Cities';
 import '../styles/footer.css'
 import '../styles/welcome.css'
 import '../styles/header.css'
+import '../styles/carousel.css'
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Cities />
+      
 
 
-
-
+      {/* <EventCarousel/> */}
       {/* <SitioWebLayout /> */}
+      {/* <HomePage /> */}
       {/* <Header />
       <Footer /> */}
-      {/* <Welcome/>
-     <Footer/> */}
+     {/*  <Welcome />
+     <Footer /> */}
     </div>
   );
 }
