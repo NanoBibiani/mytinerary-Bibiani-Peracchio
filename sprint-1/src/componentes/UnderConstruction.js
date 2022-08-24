a6dd137d051a8e7edd29bc9caf84ba0e640b8a0e
jojope38 <peracchiojoaquin16@gmail.com>
2022-08-23 22:24:30 -0300

    created component new cities and yours routes

--- sprint-1/src/componentes/UnderConstruction.js
+++ sprint-1/src/componentes/UnderConstruction.js
@@ -1,13 +1,16 @@
 import CallToAction from "./CallToAction"
-
+import "../styles/under.css"
 export default function UnderConstruction(){
 
     return(
 
-    <>
-    <CallToAction linkTo='#' TextButon='404' Clases ="button"/>
-    <h4 className="underText">not found  or under contruction page</h4>
-    <CallToAction linkTo='/' TextButon='Go-back home' Clases ="button"/>
-    </>
+    
+    <div className="underConst">
+        <CallToAction linkTo='#' TextButon='404' Clases ="button"/>
+        <h4 className="underText">not found  or under contruction page</h4>
+        <CallToAction linkTo='/' TextButon='Go-back home' Clases ="button"/>
+
+    </div>
+    
     )
 }
