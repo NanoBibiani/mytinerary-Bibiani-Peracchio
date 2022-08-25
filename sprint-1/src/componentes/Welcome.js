1620b7047a8b394a9fd6e03e028472608ab6a8aa
NanoBibiani <nbibiani70@gmail.com>
2022-08-25 01:26:02 -0300

    cambio ruta de link

--- sprint-1/src/componentes/Welcome.js
+++ sprint-1/src/componentes/Welcome.js
@@ -6,7 +6,7 @@ export default function Welcome(){
             <h1>App MyTernary</h1>
             <p className = "slogan">The only rule of the trip is: don't come back as you left. come back different</p>
             {/* <button>Go to App</button> */}
-            <CallToAction linkTo="/" TextButon='Go to App' Clases ="button"/>
+            <CallToAction linkTo="/home" TextButon='Go to App' Clases ="button"/>
         </div>
     )
 }
